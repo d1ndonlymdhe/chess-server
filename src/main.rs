3367b6aa672c45134bd9f050961d7702c147a6f6
d1ndonlymdhe <ayushshrestha8920@protonmail.com>
2023-09-08 20:20:07 +0545

    use 0.0.0.0

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ async fn get_ws(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
     // let server = server::ChessServer::new().start();
-    let server_addr = "127.0.0.1";
+    let server_addr = "0.0.0.0";
     let server_port = 8080;
     let app = HttpServer::new(move || {
         let cors = Cors::default()
